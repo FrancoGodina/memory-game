f5d229110878a55228022a0b0ed2c9fe970f3c1f
franco <nahuelgodina@gmail.com>
2022-04-26 15:27:00 -0300

    build PlanetCard display

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,6 +1,13 @@
-function PlanetCard() {
+function PlanetCard(props) {
     return (
-        
+        <div onClick={() => props.handleClick(props.id)}>
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="img-content">
+                <h3>{props.name}</h3>
+            </div>
+        </div>
     )
 }
 
